e9cf5c5d2273100f541daa2ca6cdac67eb2901ba
hansep <peter.hansen@charite.de>
2019-10-24 09:25:33 -0400

    Revised R script f

--- rscripts/create_permutation_plot.r
+++ rscripts/create_permutation_plot.r
@@ -30,9 +30,19 @@ PREFIX <- "../results_1/nora/treated/nora_treated_r2"
 #PREFIX <- "../results_1/nora/washoff/nora_washoff_r2"
 #PREFIX <- "../results_1/nora/washoff/nora_washoff_alt"
 
+PREFIX <- "../mifsud_alt_iter_1000"
+#PREFIX <- "../schoenefelder_alt_iter_1000"
+#PREFIX <- "../chesi_bmp2_alt_iter_1000"
+PREFIX <- "../chesi_hepg2_alt_iter_1000"
+PREFIX <- "../nora_untreated_alt_iter_1000"
+PREFIX <- "../nora_treated_alt_iter_1000"
+PREFIX <- "../nora_washoff_alt_iter_1000"
+
+
+
 
 f_name<-paste(PREFIX, "_permutation_plot.pdf", sep="")
-cairo_pdf(f_name, width=5.5, height=5.5)
+cairo_pdf(f_name, width=4.5, height=4.5)
 
 f_name<-paste(PREFIX, "_permutation_summary.txt", sep="")
 SUMMARY<-read.table(f_name, header=T)
@@ -46,11 +56,14 @@ if(10000<=INTERACTION_NUM){
   INTERACTION_NUM <- format(as.numeric(INTERACTION_NUM), big.mark=",")
 }
 NOMINAL_ALPHA <- SUMMARY[,3]
-ITER_NUM <- SUMMARY[,4]
-NSIG_OBSERVED <- SUMMARY[,5]
-NSIG_PERMUTED_MEAN <- round(SUMMARY[,6])
+ITER_NUM <- SUMMARY[,2]
+NSIG_OBSERVED <- SUMMARY[,10]
+NSIG_PERMUTED_MEAN <- round(SUMMARY[,11])
 NSIG_RATIO_OBSERVED_PERMUTED <- round(NSIG_OBSERVED/NSIG_PERMUTED_MEAN,2)
-BETTER_THAN_OBSERVED <- SUMMARY[,7]
+BETTER_THAN_OBSERVED <- SUMMARY[,12]
+N_TOTAL_INTERACION <- SUMMARY[,5]
+N_INDEF_INTERACION <- SUMMARY[,8]
+N_UNDIR_INTERACION <- SUMMARY[,9]
 
 # compute Z score
 p_mean <- mean(t(INTERACTION_NUMBERS))
@@ -70,11 +83,14 @@ LEGEND_1 <- paste("Permuted interactions:", INTERACTION_NUM)
 LEGEND_2 <- paste("Nominal alpha:", NOMINAL_ALPHA)
 LEGEND_3 <- paste("Iterations:", ITER_NUM)
 LEGEND_4 <- paste("Better than observerd:", BETTER_THAN_OBSERVED)
-LEGEND_5 <- paste("Observed significant interactions:", NSIG_OBSERVED)
-LEGEND_6 <- paste("Mean permuted significant interactions:", NSIG_PERMUTED_MEAN)
-LEGEND_7 <- paste("Ratio observed/permuted:", NSIG_RATIO_OBSERVED_PERMUTED)
-LEGEND_8 <- paste("Z score:", z_score)
-legend("topright", legend=c(LEGEND_1, LEGEND_2, LEGEND_3, LEGEND_4, LEGEND_5, LEGEND_6, LEGEND_7, LEGEND_8),
-       col=c("black", "black", "black", "black", "black", "black", "black", "black"), pch=c(20,20,20,20,20,20,20,20), cex=0.8)
+LEGEND_5 <- paste("Total number of interactions:", N_TOTAL_INTERACION)
+LEGEND_6 <- paste("Number of indefinable interactions:", N_INDEF_INTERACION)
+LEGEND_7 <- paste("Number of undirected interactions:", N_UNDIR_INTERACION)
+LEGEND_8 <- paste("Number of significant directed interactions:", NSIG_OBSERVED)
+LEGEND_9 <- paste("Mean permuted significant interactions:", NSIG_PERMUTED_MEAN)
+LEGEND_10 <- paste("Ratio observed/permuted:", NSIG_RATIO_OBSERVED_PERMUTED)
+LEGEND_11 <- paste("Z score:", z_score)
+legend("topright", legend=c(LEGEND_1, LEGEND_2, LEGEND_3, LEGEND_4, LEGEND_5, LEGEND_6, LEGEND_7, LEGEND_8, LEGEND_9, LEGEND_10, LEGEND_11),
+       col=c("black", "black", "black", "black", "black", "black", "black", "black", "black", "black", "black"), pch=c(20,20,20,20,20,20,20,20,20,20,20), cex=0.6)
 
 dev.off()
