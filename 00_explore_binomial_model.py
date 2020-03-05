88c6bc62b44b3845287b3825fcc3236019a54b4e
phansen <peter.hansen@charite.de>
2020-03-05 19:19:43 +0100

    Revised and extended Python script for exploration of binomial P-value.

--- 00_explore_binomial_model.py
+++ 00_explore_binomial_model.py
@@ -2,12 +2,19 @@
 
 """
 This script is to investigate the relationship between signal strength, measured as the total number of read pairs,
-and the directionality of interactions. For this purpose, a specified number of interactions is simulated,
-whereby the the total numbers of read pairs are drawn from a uniform distribution.
-For individual interactions with n read pairs, the number of simple read pairs is drawn from a binomial distribution
-according our null model and the number of twisted read pairs is set to n minus the number of simple read pairs.
-Subsequently, the simulated interactions are evaluated for statistical significance at a specified significance
-threshold using our null model. Finally, the number of significant interactions for each n is plotted.
+and the binomial model for directionality of interactions.
+
+For this purpose, a specified number of interactions is simulated, whereby the the total numbers of read pairs are drawn
+from a uniform distribution. For individual interactions with n read pairs, the number of simple read pairs is drawn
+from a binomial distribution with p = 0.5 (null model) and the number of twisted read pairs is set to n minus the number
+of simple read pairs. Subsequently, the simulated interactions are evaluated for statistical significance given the null
+model and a specified significance threshold. Finally, a plot for the number of significant simulated interactions for
+each n is generated and, in addition, the corresponding numbers are written to a tab separated text file.
+
+If a Diachromatic interaction files is specified, the numbers of significant empirical interactions for each n will also
+be determined and written to file.
+
+The two tab separated output text files can be used to reproduce Figure S1 in R.
 """
 
 from collections import defaultdict
@@ -15,125 +22,161 @@ import argparse
 from scipy.stats import binom
 import numpy as np
 import matplotlib.pyplot as plt
+import diachrscripts_toolkit as dclass
+
 
 ### Parse command line
 ######################
 
-parser = argparse.ArgumentParser(description='Determine significance of numbers of x:y interactions with x+y=k.')
+parser = argparse.ArgumentParser(description='Explore binomial model using simuated and real interactions.')
 parser.add_argument('--out-prefix', help='Prefix for output.', default='OUTPREFIX')
 parser.add_argument('--i-num', help='Number of simulated interactions')
 parser.add_argument('--n-max', help='Simulate interactions with 1 to n read pairs.')
-parser.add_argument('--p-value-cutoff', help='Nominal alpha. P-value threshold used for the classification of directed and undirected interactions.', default=0.05)
+parser.add_argument('--p-value-cutoff', help='P-value threshold used for the classification of directed and undirected interactions.', default=0.05)
+parser.add_argument('--diachromatic-interaction-file', help='Diachromatic interaction file.')
 
 args = parser.parse_args()
 out_prefix = args.out_prefix
 n_max = int(args.n_max)
 i_num = int(args.i_num)
 p_value_cutoff = float(args.p_value_cutoff)
+diachromatic_interaction_file = args.diachromatic_interaction_file
+
 
 print("[INFO] " + "Input parameters")
 print("\t[INFO] --out_prefix: " + out_prefix)
 print("\t[INFO] --i-num: " + str(i_num))
 print("\t[INFO] --n-max: " + str(n_max))
 print("\t[INFO] --p-value-cutoff: " + str(p_value_cutoff))
+if diachromatic_interaction_file != None:
+    print("\t[INFO] --diachromatic-interaction-file: " + diachromatic_interaction_file)
 
 
 ### Define auxiliary functions
 ##############################
 
-def random_numbers_dict(n_dict):
-    """
-    This function can be used to generate all random numbers required for one permutation at once,
-    which is far more efficient that generating random numbers one by one.
-
-    :param n_dict: Dictionary with read pair numbers as keys and interaction numbers as values.
-    :return: Dictionary with read pair numbers as keys and lists of random numbers of simple read pairs as values.
-    """
-    random_numbers_dict = {}
-    for n, num in n_dict.items():
-        random_numbers_dict[n] = list(binom.rvs(n, p=0.5, size=num))
-    return random_numbers_dict
-
+# Dictionary that keeps track of already calculated P-values
+#    key - a string like 2-7
+#    value - our corresponding binomial p-value
+#    note -- use this as a global variable in this script!
 pval_memo = defaultdict(float)
 
-def binomial_p_value(n_simple, n_twisted):
+def binomial_p_value(simple_count, twisted_count):
     """
     Locally defined method for the calculation of the binomial P-value that uses a dictionary that keeps track of
     P-values that already have been calculated.
 
-    :param n_simple: number of simple read pairs
-    :param n_twisted: Number of twisted read pairs
+    :param simple_count: Number of simple read pairs
+    :param twisted_count: Number of twisted read pairs
     :return: Binomial P-value
     """
-    key = "{}-{}".format(n_simple, n_twisted)
+
+    # Create key from simple and twisted read pair counts
+    key = "{}-{}".format(simple_count, twisted_count)
+
+    # Check whether a P-value for this combination of simple and twisted counts has been calculated already
     if key in pval_memo:
         return pval_memo[key]
     else:
-        if n_simple < n_twisted:
-            p_value = 1 - binom.cdf(n_twisted - 1, n_simple + n_twisted, 0.5)
-            pval_memo[key] = p_value
-            return p_value
-        else:
-            p_value = 1 - binom.cdf(n_simple - 1, n_simple + n_twisted, 0.5)
-            pval_memo[key] = p_value
-            return p_value
+        # Calculate P-value and add to dictionary
+        p_value = dclass.calculate_binomial_p_value(simple_count, twisted_count)
+        pval_memo[key] = p_value
+        return p_value
+
+
+### Prepare variables, data structures and streams for output files
+###################################################################
+
+# Dictionary that stores the numbers of significant interactions for each n
+N_DICT_SIM = {}
+N_DICT_EMP = {}
+
+# List containing counts of significant simple and twisted interactions for each n
+signum_list = [0] * (n_max + 1)
+
+# Name for PDF file with significant simulated interactions versus n plot
+pdf_name = out_prefix + "_sig_interactions_vs_uniform_n.pdf"
+
+# Name for text file with significant empirical interactions for each n
+sim_tab_file_name = out_prefix + "_sig_interactions_vs_uniform_n.tab"
+sim_tab_stream_name = open(sim_tab_file_name, 'wt')
+
+if diachromatic_interaction_file != None:
+
+    # Name for text file with significant empirical interactions for each n
+    emp_tab_file_name = out_prefix + "_sig_interactions_vs_empirical_n.tab"
+    emp_tab_stream_name = open(emp_tab_file_name, 'wt')
 
 
 ### Start execution
 ###################
 
-print("[INFO] " + "Generating numbers of simple and twisted read pairs ...")
-n_dict = {} # dictionary that stores the numbers of interactions with n read pairs; is used as input for 'random_numbers_dict()' to generate random counts efficiently
-random_n_vec = np.random.randint(low = 1, high = n_max+1, size = i_num) # n has a uniform distribution
+print("[INFO] " + "Generating random numbers of simple and twisted read pairs ...")
+
+# Random vector for n with uniform distribution
+random_n_vec = np.random.randint(low = 1, high = n_max  + 1, size = i_num)
 for n in random_n_vec:
-    if n in n_dict:
-        n_dict[n] += 1
+    if n in N_DICT_SIM:
+        N_DICT_SIM[n] += 1
     else:
-        n_dict[n] = 1
+        N_DICT_SIM[n] = 1
 
-for i in range(1,n_max):
-    pass
-    #print("n=" + str(i) + "\t" + str(n_dict[i]))
-random_numbers_dict = random_numbers_dict(n_dict)
-# 'random_numbers_dict' now contains arrays of random simple read numbers for each n
-
-for N in random_numbers_dict:
-    arr = [0] * (N+1)
-    for n_simple in random_numbers_dict[N]:
-        arr[n_simple] += 1
 
 print("[INFO] " + "Counting significant interactions for each n ...")
-signum_list = [0] * (n_max + 1)
-signum_list_simple = [0] * (n_max + 1)
-signum_list_twisted = [0] * (n_max + 1)
-n_list = [0] * (n_max + 1)
-for N in range(1, n_max + 1):
-    if N in random_numbers_dict:
-        if N%10 == 0:
-            print(str(N))
-        for n_simple in random_numbers_dict[N]:
-            n_twisted = N - n_simple
-            p_val = binomial_p_value(n_simple, n_twisted)
-            if p_val <= p_value_cutoff:
-                signum_list[N] += 1
-                if n_simple < n_twisted:
-                    signum_list_simple[N] += 1
-                else:
-                    signum_list_twisted[N] += 1
 
+# Iterate dictionary with numbers of interactions for each read pair number n
+for n, i in N_DICT_SIM.items():
+
+    # Generate random simple read pair counts for current n
+    simple_count_list = list(binom.rvs(n, p = 0.5, size = i))
+
+    for simple_count in simple_count_list:
+
+        # Get twisted count
+        twisted_count = n - simple_count
+
+        # Get binomial P-value
+        key = "{}-{}".format(simple_count, twisted_count)
+        if key in pval_memo:
+            pv = pval_memo[key]
+        else:
+            pv = binomial_p_value(simple_count, twisted_count)
+            pval_memo[key] = pv
+
+        # Count significant interactions for current n
+        if pv <= p_value_cutoff:
+            signum_list[n] += 1
+
+
+print("[INFO] " + "Generating plot: Significant simulated interactions versus n ...")
 plt.plot(signum_list)
 plt.grid(True)
-plt.xlabel("N read pairs")
-plt.ylabel("#Significant interactions")
-sub_title = "#Interactions: " + str(i_num) + " | Max n: " + str(n_max) + " | P-value cutoff: " + str(p_value_cutoff)
+plt.xlabel("n")
+plt.ylabel("# Significant interactions")
+sub_title = "# Interactions: " + str(i_num) + " | Max n: " + str(n_max) + " | P-value cutoff: " + str(p_value_cutoff)
 plt.suptitle(sub_title)
-figure_name = out_prefix + "_uniform_n_sig_interaction_dist.pdf"
-plt.savefig(figure_name, format = "pdf")
+plt.savefig(pdf_name, format = "pdf")
 
-print("[INFO] " + "Printing numbers of significant interactions to text file ...")
-file_name = out_prefix + "_num_of_sig_for_each_n.tab"
-f_output = open(file_name, 'wt')
-for N in range(1, n_max + 1):
-    f_output.write(str(N) + "\t" + str(signum_list[N]) + "\t" + str(signum_list_simple[N]) + "\t" + str(signum_list_twisted[N]) + "\t" + str(n_dict[N]) + "\n")
 
-f_output.close()
+print("[INFO] " + "Writing numbers of significant simulated interactions for each n to text file ...")
+for n in range(0, n_max + 1):
+    try:
+        sim_tab_stream_name.write(str(n) + "\t" + str(signum_list[n]) + "\t" + str(N_DICT_SIM[n]) + "\n")
+    except KeyError:
+        sim_tab_stream_name.write(str(n) + "\t" + str(signum_list[n]) + "\t" + str(0) + "\n")
+sim_tab_stream_name.close()
+
+
+if diachromatic_interaction_file != None:
+
+    # Count significant interactions in empirical data for each n
+    N_DICT_EMP = dclass.get_n_dict(diachromatic_interaction_file, 'ALL', 20000, 0.0039)
+
+    print("[INFO] " + "Writing numbers of significant empirical interactions for each n to text file ...")
+    for n in range(0, 2000):
+        try:
+            emp_tab_stream_name.write(str(n) + "\t" + str(N_DICT_EMP[n]) + "\n")
+        except KeyError:
+            emp_tab_stream_name.write(str(n) + "\t" + str(0) + "\n")
+
+    emp_tab_stream_name.close()
