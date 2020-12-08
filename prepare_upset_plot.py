4a26bc020824ddb8ad216ba96bb76e663d23ba0b
hansep <peter.hansen@charite.de>
2020-12-08 04:41:16 -0500

    Archived and removed old scripts.

--- prepare_upset_plot.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-"""
-This script takes as input a list of enhanced interaction files and associated names (cell types). For a given
-interaction, i.e. digest pair, it is determined in which interaction set it occurs.
-"""
