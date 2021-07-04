110587511e990b3d91f1b8a1a2a456abd39c4139
Peter Robinson <peter.robinson@jax.org>
2021-07-04 17:04:07 -0400

    merged with current develop

--- diachr/dtadvis.py
+++ diachr/dtadvis.py
@@ -18,6 +18,7 @@ class Digest:
     """
     Represent a BED3 version of a digest
     """
+
     def __init__(self, chrom, begin, end):
         if not isinstance(chrom, str):
             raise ValueError("chrom must be string")
@@ -26,7 +27,6 @@ class Digest:
         self._chrom = chrom
         self._begin = begin
         self._end = end
-
 
     @property
     def chrom(self):
@@ -57,7 +57,7 @@ class Digest:
 
 
 class Interaction:
-    def __init__(self, digestA, digestB, statusA, statusB, ST ) -> None:
+    def __init__(self, digestA, digestB, statusA, statusB, ST) -> None:
         if not (isinstance(digestA, Digest) and isinstance(digestB, Digest)):
             raise ValueError("Did not pass Digest object")
         if statusA != 'E' and statusA != 'N':
@@ -98,12 +98,11 @@ class Interaction:
 
     @property
     def percentage(self):
-        p = int(100*self._simple / (self._simple + self._twisted))
+        p = int(100 * self._simple / (self._simple + self._twisted))
         if p > 99:
             return 99
         else:
             return p
-
 
 
 class DirectedTadVisualizer:
@@ -117,19 +116,19 @@ class DirectedTadVisualizer:
             raise ValueError("Invalid directed interaction file {}".format(fname))
         self._fname = fname
         self._figure_size = 10
-        c1='#1f77b4' #blue
-        c2='green' #green
+        c1 = '#1f77b4'  # blue
+        c2 = 'green'  # green
         red = Color("red")
-        self._colors = list(red.range_to(Color(c1),100))
-
+        self._colors = list(red.range_to(Color(c1), 100))
 
     def extract_interactions(self, chrom, begin, end, fname='dtadvis-out.tsv'):
         if not isinstance(chrom, str):
             raise ValueError("chrom must be a string")
-        if not isinstance(begin, int) or not isinstance(end,int):
+        if not isinstance(begin, int) or not isinstance(end, int):
             raise ValueError("begin and end must be integers")
         interaction_set = DiachromaticInteractionSet()
         inter_dict = defaultdict(DiachromaticInteraction)
+        LEN = len(chrom)
         if self._fname.endswith(".gz"):
             with gzip.open(self._fname, 'rt') as fp:
                 n_lines = 0
@@ -137,6 +136,8 @@ class DirectedTadVisualizer:
                     n_lines += 1
                     if n_lines % 1000000 == 0:
                         print("\t[INFO] Parsed " + "{:,}".format(n_lines) + " interaction lines ...")
+                    if line[:LEN] != chrom:
+                        continue  # skip lines from other chromosomes
                     d_inter = interaction_set._parse_line(line)
                     # Only keep interactions completely within the region we want to visualize
                     if d_inter.chrA == chrom and d_inter.chrB == chrom:
@@ -144,7 +145,7 @@ class DirectedTadVisualizer:
                             continue
                     if d_inter.key in inter_dict:
                         inter_dict[d_inter.key].append_interaction_data(simple=d_inter.n_simple,
-                                                                              twisted=d_inter.n_twisted)
+                                                                        twisted=d_inter.n_twisted)
                     else:
                         inter_dict[d_inter.key] = d_inter
         else:
@@ -154,7 +155,6 @@ class DirectedTadVisualizer:
             for ia in inter_dict.values():
                 fh.write(ia.get_diachromatic_interaction_line() + "\n")
         fh.close()
-
 
     def pos_to_coordinate(self, pos):
         return (pos - self._min) * self._factor
@@ -169,37 +169,35 @@ class DirectedTadVisualizer:
             raise ValueError("Not an Interaction")
         if not isinstance(digest_to_coords, dict):
             raise ValueError("Not a dictionary")
-        TANGENT_45 = math.tan(math.pi/4) # The lines go up at 45 degree angles
+        TANGENT_45 = math.tan(math.pi / 4)  # The lines go up at 45 degree angles
         # a,b,c,d are coordinates of the two digests on the X axis
         a = self.pos_to_coordinate(interaction.digestA.begin)
         b = self.pos_to_coordinate(interaction.digestA.end)
         c = self.pos_to_coordinate(interaction.digestB.begin)
         d = self.pos_to_coordinate(interaction.digestB.end)
         # bc is the point where lines from b and c meet
-        bc_x = b + 0.5 * (c-b)
+        bc_x = b + 0.5 * (c - b)
         # bc is the length of the segment from b to bc_x
         bc = bc_x - b
         # tan = opposite/adjacent
         # opposite = tan(45)*adjacent
         bc_y = TANGENT_45 * bc
         # ad is the point where lines from a and d meet
-        ad_x = a + 0.5 * (d-a)
+        ad_x = a + 0.5 * (d - a)
         ad = ad_x - a
         ad_y = TANGENT_45 * ad
         # ac is the point where lines from a and c meet
-        ac_x = a + 0.5 * (c-a)
+        ac_x = a + 0.5 * (c - a)
         ac = ac_x - a
         ac_y = TANGENT_45 * ac
         # bd is the point where lines from b and d meet
-        bd_x = b + 0.5 * (d -b)
+        bd_x = b + 0.5 * (d - b)
         bd = bd_x - b
         bd_y = TANGENT_45 * bd
-        poly =  Polygon([(bc_x, bc_y), (bd_x, bd_y), (ad_x, ad_y),(ac_x, ac_y)])
+        poly = Polygon([(bc_x, bc_y), (bd_x, bd_y), (ad_x, ad_y), (ac_x, ac_y)])
         i = interaction.percentage
         color = self._colors[i].get_rgb()
-        return PolygonPatch(polygon=poly,ec=color)
-
-
+        return PolygonPatch(polygon=poly, ec=color)
 
     def create_visualization(self, fname):
         """
@@ -216,9 +214,9 @@ class DirectedTadVisualizer:
                 digestB = Digest(chrom=fields[4], begin=int(fields[5]), end=int(fields[6]))
                 digest_set.add(digestA)
                 digest_set.add(digestB)
-                ia = Interaction(digestA=digestA, digestB=digestB, statusA=fields[3], statusB=fields[7],ST=fields[8])
+                ia = Interaction(digestA=digestA, digestB=digestB, statusA=fields[3], statusB=fields[7], ST=fields[8])
                 interaction_list.append(ia)
-      
+
         digest_list = list(digest_set)
         digest_list.sort(key=lambda x: x.begin, reverse=False)
         self._min = min([x.begin for x in digest_list])
@@ -231,9 +229,9 @@ class DirectedTadVisualizer:
             digest_to_coords[d] = self.digest_to_coordinates(d)
         print("[INFO] Got {} digests".format(len(digest_set)))
         print("[INFO] Got {} interactions".format(len(interaction_list)))
-        fig = plt.figure(1, figsize=(10,10))
+        fig = plt.figure(1, figsize=(10, 10))
         ax = fig.add_subplot(111)
-       
+
         ax.set_title('Directed Interactions')
         xrange = [0, 10]
         yrange = [0, 10]
@@ -242,8 +240,8 @@ class DirectedTadVisualizer:
         for ia in interaction_list:
             polypatch = self.interaction_to_polygon(interaction=ia, digest_to_coords=digest_to_coords)
             ax.add_patch(polypatch)
-        #ax.set_xticks(range(*xrange) + [xrange[-1]])
-        #ax.set_ylim(*yrange)
-        #ax.set_yticks(range(*yrange) + [yrange[-1]])
+        # ax.set_xticks(range(*xrange) + [xrange[-1]])
+        # ax.set_ylim(*yrange)
+        # ax.set_yticks(range(*yrange) + [yrange[-1]])
         ax.set_aspect(1)
         plt.show()
