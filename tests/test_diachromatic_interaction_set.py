6b10a710bdb1c3456dd5f579b30fe6f6aacf65a4
hansep <peter.hansen@charite.de>
2021-07-20 09:56:21 -0400

    Added test for HT transformation of the four read pair counts (#41).

--- tests/test_diachromatic_interaction_set.py
+++ tests/test_diachromatic_interaction_set.py
@@ -21,10 +21,12 @@ class TestDiachromaticInteractionSet(TestCase):
 
         cls.interaction_set_1 = DiachromaticInteractionSet()
         cls.interaction_set_2 = DiachromaticInteractionSet()
+        cls.interaction_set_3 = DiachromaticInteractionSet()
         test_dir = os.path.dirname(__file__)
         interaction_file = os.path.join(test_dir, "data/test_04/diachromatic_interaction_file.tsv")
         cls.interaction_set_1.parse_file(interaction_file)
         cls.interaction_set_2.parse_file(interaction_file)
+        cls.interaction_set_3.parse_file(interaction_file)
 
     def test_shuffle_inter_dict(self):
         """
@@ -68,4 +70,49 @@ class TestDiachromaticInteractionSet(TestCase):
                 self.assertEqual('chr10', d_inter.chrA)
             if i == 2:
                 self.assertEqual('chr7', d_inter.chrA)
+            i += 1
+
+    def test_transform_4rp_counts_to_heaviest_two_rule(self):
+        """
+        Test whether the heaviest two transformation of the four read pair counts works as expected.
+        """
+
+        i = 0
+        for d_inter in self.interaction_set_3.interaction_list:
+            if i == 0:
+                # Read pair counts before transformation: 100:0:1:0
+                self.assertEqual(100, d_inter._simple_1)
+                self.assertEqual(0, d_inter._simple_2)
+                self.assertEqual(1, d_inter._twisted_1)
+                self.assertEqual(0, d_inter._twisted_2)
+                self.assertEqual(100, d_inter.n_simple)
+                self.assertEqual(1, d_inter.n_twisted)
+            if i == 18:
+                # Read pair counts before transformation: 50:0:51:0
+                self.assertEqual(50, d_inter._simple_1)
+                self.assertEqual(0, d_inter._simple_2)
+                self.assertEqual(51, d_inter._twisted_1)
+                self.assertEqual(0, d_inter._twisted_2)
+                self.assertEqual(50, d_inter.n_simple)
+                self.assertEqual(51, d_inter.n_twisted)
+            i += 1
+        self.interaction_set_3.transform_4rp_counts_to_heaviest_two_rule()
+        i = 0
+        for d_inter in self.interaction_set_3.interaction_list:
+            if i == 0:
+                # Read pair counts after transformation: 100:1:0:0
+                self.assertEqual(100, d_inter._simple_1)
+                self.assertEqual(1, d_inter._simple_2)
+                self.assertEqual(0, d_inter._twisted_1)
+                self.assertEqual(0, d_inter._twisted_2)
+                self.assertEqual(101, d_inter.n_simple)
+                self.assertEqual(0, d_inter.n_twisted)
+            if i == 18:
+                # Read pair counts after transformation: 51:50:0:0
+                self.assertEqual(51, d_inter._simple_1)
+                self.assertEqual(50, d_inter._simple_2)
+                self.assertEqual(0, d_inter._twisted_1)
+                self.assertEqual(0, d_inter._twisted_2)
+                self.assertEqual(101, d_inter.n_simple)
+                self.assertEqual(0, d_inter.n_twisted)
             i += 1
