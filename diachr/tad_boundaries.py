e81377255c4570ed0eaa183f57c54e6bcae15e62
hansenp <peter.hansen@jax.org>
2021-08-27 05:26:18 -0400

    Added function for boundaries spanned by region and tests.

--- diachr/tad_boundaries.py
+++ diachr/tad_boundaries.py
@@ -2,25 +2,26 @@ from bisect import bisect_left
 
 
 class TadBoundarySet:
-
     # Class to store TAD boundaries with a function that returns the next boundary for a given coordinate
 
     # Attributes
 
-    n_tads = 0   # Total number of TAD boundaries
+    n_tads = 0  # Total number of TAD boundaries
 
-    chr_dict = {}   # Dictionary that has a list of boundary coordinates for each chromosome
+    chr_dict = {}  # Dictionary that has a list of boundary coordinates for each chromosome
 
     # Initializer
 
-    def __init__(self, tad_region_bed_file):
+    def __init__(self, tad_boundary_bed_file):
 
-        with open(tad_region_bed_file, 'rt') as fp:
+        self.chr_dict = {}
+
+        with open(tad_boundary_bed_file, 'rt') as fp:
             next(fp)
             for line in fp:
 
                 # Parse line
-                chr_key, boundary_1, boundary_2 = line.rstrip().split('\t')
+                chr_key, boundary_1, boundary_plus_one = line.rstrip().split('\t')
 
                 # Count TAD boundaries
                 self.n_tads = self.n_tads + 2
@@ -28,9 +29,8 @@ class TadBoundarySet:
                 # Add boundary coordinates to dictionary
                 if chr_key in self.chr_dict:
                     self.chr_dict[chr_key].append(int(boundary_1))
-                    self.chr_dict[chr_key].append(int(boundary_2))
                 else:
-                    self.chr_dict[chr_key] = [int(boundary_1), int(boundary_2)]
+                    self.chr_dict[chr_key] = [int(boundary_1)]
 
         # Sort lists with coordinates for each chromosome
         for chr_key in self.chr_dict:
@@ -39,7 +39,7 @@ class TadBoundarySet:
     def get_nearest_tad_boundary(self, chr_key, coord):
 
         # Get the index of x (if in the list) or the index of next larger coordinate
-        boundary_to_the_right_idx = bisect_left(self.chr_dict[chr_key],int(coord))
+        boundary_to_the_right_idx = bisect_left(self.chr_dict[chr_key], int(coord))
 
         if len(self.chr_dict[chr_key]) <= boundary_to_the_right_idx:
             # There is no boundary to the right
@@ -53,10 +53,11 @@ class TadBoundarySet:
             # coord is a TAD boundary and the distance is zero
             return coord
         else:
-            # coord is between two TAD boundaries, one to the left and one to the right
+            # coord is between two TAD boundaries
             boundary_to_the_left_idx = boundary_to_the_right_idx - 1
 
-            if (self.chr_dict[chr_key][boundary_to_the_right_idx] - coord) < (coord - self.chr_dict[chr_key][boundary_to_the_left_idx]):
+            if (self.chr_dict[chr_key][boundary_to_the_right_idx] - coord) < (
+                    coord - self.chr_dict[chr_key][boundary_to_the_left_idx]):
                 # The boundary to the right is closer
                 return self.chr_dict[chr_key][boundary_to_the_right_idx]
             else:
@@ -70,7 +71,7 @@ class TadBoundarySet:
             return -1
 
         # Determine distance to next TAD boundary
-        nearest_tad_boundary_coord = self.get_nearest_tad_boundary(chr_key,coord)
+        nearest_tad_boundary_coord = self.get_nearest_tad_boundary(chr_key, coord)
         if coord <= nearest_tad_boundary_coord:
             return nearest_tad_boundary_coord - coord
         else:
@@ -89,4 +90,28 @@ class TadBoundarySet:
         else:
             return coord - nearest_tad_boundary_coord
 
+    def get_number_of_boundaries_spanned_by_region(self, chr_key, sta, end):
 
+        # Check whether there are TAD boundaries on this chromosome
+        if chr_key not in self.chr_dict:
+            return -1
+
+        num_of_spanned_boundaries = 0
+
+        # Get the index of sta (if in the list) or the index of next larger coordinate
+        boundary_to_the_right_idx = bisect_left(self.chr_dict[chr_key], int(sta))
+
+        if len(self.chr_dict[chr_key]) <= boundary_to_the_right_idx:
+            # There is no boundary to the right
+            return num_of_spanned_boundaries
+
+        if self.chr_dict[chr_key][boundary_to_the_right_idx] == sta:
+            # sta is a TAD boundary
+            num_of_spanned_boundaries += 1
+            boundary_to_the_right_idx += 1
+
+        while self.chr_dict[chr_key][boundary_to_the_right_idx] < end:
+            num_of_spanned_boundaries += 1
+            boundary_to_the_right_idx += 1
+
+        return num_of_spanned_boundaries
