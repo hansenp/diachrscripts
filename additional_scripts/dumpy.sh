d8574aff8aabcd5f963d17e6f3a656ca724796fc
hansenp <peter.hansen@jax.org>
2022-06-20 02:00:34 -0400

    Revised dumpy.sh.

--- additional_scripts/dumpy.sh
+++ additional_scripts/dumpy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Usage examples
 # dumpy.sh  MIF_R1 MIF_R1 "ERR436029"
-# dumpy.sh  MIF_R2 MIF_R2 "ERR436028 ERR436030 ERR436033"
-# dumpy.sh  MIF_R3 MIF_R3 "ERR436031 ERR436026"
+# dumpy.sh  MIF_R2 MIF_R2 "ERR436028;ERR436030;ERR436033"
+# dumpy.sh  MIF_R3 MIF_R3 "ERR436031;ERR436026"
 OUT_DIR=$1
 OUT_PREFIX=$2
 SRR_LIST=$3
@@ -12,6 +12,7 @@ mkdir $OUT_DIR
 > $OUT_DIR/$OUT_PREFIX\_2.fastq # Reverse
 > $OUT_DIR/$OUT_PREFIX\_md5.txt
 # Iterate through list of identifiers
+IFS=";"
 for SRR in $SRR_LIST;
 do
   # Download and append
@@ -28,8 +29,8 @@ do
   rm $OUT_DIR/$SRR\_2.fastq.gz
 done
 # Compress concatenated files
-gzip $OUT_DIR/$OUT_PREFIX\_1.fastq # Forward
+gzip $OUT_DIR/$OUT_PREFIX\_1.fastq.gz # Forward
 echo $OUT_PREFIX >> $OUT_DIR/md5.txt
 md5sum $OUT_DIR/$OUT_PREFIX\_1.fastq >> $OUT_DIR/$OUT_PREFIX\_md5.txt
-gzip $OUT_DIR/$OUT_PREFIX\_2.fastq # Reverse
+gzip $OUT_DIR/$OUT_PREFIX\_2.fastq.gz # Reverse
 md5sum $OUT_DIR/$OUT_PREFIX\_2.fastq >> $OUT_DIR/$OUT_PREFIX\_md5.txt
